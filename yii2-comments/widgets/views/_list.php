d9945480297f82ffca7b441660dc3c7ff6057abe
Nick <nikakharadze@yahoo.com>
2019-02-12 18:05:48 +0400

    return comment script as it was before

--- yii2-comments/widgets/views/_list.php
+++ yii2-comments/widgets/views/_list.php
@@ -9,23 +9,21 @@ use yii2mod\editable\Editable;
 /* @var $maxLevel null|integer comments max level */
 ?>
 <li class="comment" id="comment-<?php echo $model->id; ?>">
-
-
-    <div class="comment-content" data-comment-content-id="<?php echo $model->id ?>">
+    <div class="comment-content" data-comment-content-id="<?php echo $model->id; ?>">
         <div class="comment-author-avatar">
             <?php echo Html::img($model->getAvatar(), ['alt' => $model->getAuthorName()]); ?>
         </div>
         <div class="comment-details">
             <div class="comment-action-buttons">
                 <?php if (Yii::$app->getUser()->can('administrator')) : ?>
-                    <?php echo Html::a('<span class="glyphicon glyphicon-trash"></span> ' . Yii::t('yii2mod.comments', 'Delete'), '#', ['data' => ['action' => 'delete', 'url' => Url::to(['/comment/default/delete', 'id' => $model->id]), 'comment-id' => $model->id]]); ?>
+                    <?php echo Html::a('<span class="glyphicon glyphicon-trash"></span> ' . Yii::t('yii2mod.comments', 'Delete'), '#', ['class' => 'delete-comment-btn', 'data' => ['action' => 'delete', 'url' => Url::to(['/comment/default/delete', 'id' => $model->id]), 'comment-id' => $model->id]]); ?>
                 <?php endif; ?>
                 <?php if (!Yii::$app->user->isGuest && ($model->level < $maxLevel || is_null($maxLevel))) : ?>
-                    <?php echo Html::a("<span class='glyphicon glyphicon-share-alt'></span> " . Yii::t('yii2mod.comments', 'Reply'), '#', ['class' => 'comment-reply', 'data' => ['action' => 'reply', 'comment-id' => $model->id]]); ?>
+                    <?php echo Html::a("<span class='glyphicon glyphicon-share-alt'></span> " . Yii::t('yii2mod.comments', 'Reply'), '#', ['class' => 'reply-comment-btn', 'data' => ['action' => 'reply', 'comment-id' => $model->id]]); ?>
                 <?php endif; ?>
             </div>
             <div class="comment-author-name">
-                <span><?php echo $model->getTest(); ?></span>
+                <span><?php echo $model->getAuthorName(); ?></span>
                 <?php echo Html::a($model->getPostedDate(), $model->getAnchorUrl(), ['class' => 'comment-date']); ?>
             </div>
             <div class="comment-body">
@@ -33,7 +31,7 @@ use yii2mod\editable\Editable;
                     <?php echo Editable::widget([
                         'model' => $model,
                         'attribute' => 'content',
-                        'url' => '/comment/default/quick-edit',
+                        'url' => Url::to(['/comment/default/quick-edit']),
                         'options' => [
                             'id' => 'editable-comment-' . $model->id,
                         ],
@@ -48,9 +46,7 @@ use yii2mod\editable\Editable;
 <?php if ($model->hasChildren()) : ?>
     <ul class="children">
         <?php foreach ($model->getChildren() as $children) : ?>
-            <li class="comment" id="comment-<?php echo $children->id; ?>">
-                <?php echo $this->render('_list', ['model' => $children, 'maxLevel' => $maxLevel]) ?>
-            </li>
+            <?php echo $this->render('_list', ['model' => $children, 'maxLevel' => $maxLevel]); ?>
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
